89a3eb9dea9e52c6e41b9457e2966b19834b7ef0
AnhKhoa2279 <Anhkhoarb500@gmail.com>
2025-08-05 17:56:08 +0700

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,14 @@
+local placeId = game.PlaceId
+if placeId == 126884695634066 then
+    -- Grow A Garden
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/Anhkhoa2279/refs/heads/main/GAG-NazuXScript.lua"))()
+elseif placeId == 2753915549 then
+    -- Blox Fruits
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/Anhkhoa2279/refs/heads/main/BF-NazuXScript.lua"))()
+elseif placeId == 116495829188952 then
+    -- Dead rails
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/Anhkhoa2279/refs/heads/main/DR-NazuXScript.lua"))()
+elseif placeId == 109983668079237 then
+    -- Steal A Brainrot
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/Anhkhoa2279/refs/heads/main/ST-NazuXScript.lua"))()
+end
