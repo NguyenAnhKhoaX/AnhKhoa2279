0d63922df994e3c7516c1312752d6cb65686f370
AnhKhoa2279 <Anhkhoarb500@gmail.com>
2025-08-09 18:11:26 +0700

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,10 @@
+local Games = {
+  [2753915549] = "https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/BF-NazuXScript.lua", -- Blox Fruits Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/BF-NazuXScript.lua", -- Blox Fruits Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/BF-NazuXScript.lua", -- Blox Fruits Sea 3
+  [126884695634066] = "https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/GAG-NazuXScript.lua", -- Grow A Garden
+  [109983668079237] = "https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/ST-NazuXScript.lua", -- Steal A Brainrot
+  [116495829188952] = "https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/DR-NazuXScript.lua", -- Dead Rails
+}
+
+return Games
