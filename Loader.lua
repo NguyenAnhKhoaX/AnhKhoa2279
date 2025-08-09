49537c0e20cd4deed0ed780251c6e8fd22dbd223
AnhKhoa2279 <Anhkhoarb500@gmail.com>
2025-08-09 18:27:19 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,13 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/GameList.lua"))()
-
-local URL = Games[game.PlaceId]
-
-if URL then
-  loadstring(game:HttpGet(URL))()
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	-- Blox Fruits
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/refs/heads/main/BF-NazuXScript.lua"))()
+elseif placeId == 126884695634066 then
+	-- Grow A Garden
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/main/GAG-NazuXScript.lua"))()
+elseif placeId == 116495829188952 or placeId == 70876832253163 then
+    -- Dead Rails
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/main/DR-NazuXScript.lua"))()
+    elseif placeId == 109983668079237
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NguyenAnhKhoaX/AnhKhoa2279/main/ST-NazuXScript.lua"))()
 end
